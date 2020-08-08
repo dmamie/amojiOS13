c90eb14b61b8de2302e5a16083dfdeb5321d5381
Dennis Müller <mdennis.dennismueller@gmail.com>
2020-08-08 22:41:37 +0200

    setting up my gthbh

---
+++ amojiOS13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amojiOS13
+//
+//  Created by Dennis Müller on 08.08.20.
+//  Copyright © 2020 Amie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
